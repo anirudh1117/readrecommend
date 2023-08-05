6b85222a452ee52f19dbd280e775c9eaf76b4490
akashelhance <akashelhance@gmail.com>
2023-08-05 12:24:07 +0530

    elchance commit

--- BookRecommend/settings.py
+++ BookRecommend/settings.py
@@ -82,15 +82,11 @@ WSGI_APPLICATION = 'BookRecommend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USERNAME'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': '127.0.0.1',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
